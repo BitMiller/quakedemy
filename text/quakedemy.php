0c985d0bd7a42010de5ae8bf61fb1f6d31b5afa1
Peter Gall <gp.bitmiller@gmail.com>
2025-03-19 05:43:16 +0100

    at250319

---
+++ text/quakedemy.php
@@ -0,0 +1,49 @@
+//0#Eleje
+return [
+¼datafile name¼ => ¼0#_.php#¼,
+¼datafile online path¼ => ¼http://bitmiller.hu/Dropbox/Akasha/_.php¼,
+¼instance DTS¼ => ¼@250314-5-1511¼,
+¼version DTS¼ => ¼@250109-4-0539¼,
+¼UTF-8 check date¼ => ¼@200226-3-2230¼,
+¼UTF-8 BOM¼ => true,
+¼UTF-8 check sample¼ => ¼áéíóöőúüűÁÉÍÓÖŐÚÜŰ¼,
+¼keywords, labels¼ => ¼¼,
+
+//1#=====//
+//0#==========//
+
+¼0&Vegyes¼ => ¼
+
+- ss@250314-5-151249
+I wrote in the dictionary app: demy
+the only find was "demystify" - it's okay to me :)
+QuakeDemy(stify)
+
+- @250315-6-0828
+> https://github.com/BitMiller/quakedemy
+GNU 3.0
+> git@github.com:BitMiller/quakedemy.git
+> https://github.com/BitMiller/quakedemy.git
+
+
+
+¼, //0#Vegyes
+
+//0#==========//
+
+¼0&Teendők¼ => ¼
+
+¼, //0#Teendők
+
+//0#==========//
+
+/*
+
+¼0&orig¼ => ¼
+
+¼, //0#orig
+
+//0#==========//
+*/
+];
+//0#Vége
